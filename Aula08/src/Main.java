d550fe721f47d921944f89058ba8630d28d45cb4
Luiz Eduardo <luiz.costa17@outlook.com>
2025-04-28 20:46:52 -0300

    Add files via upload

---
+++ Aula08/src/Main.java
@@ -0,0 +1,23 @@
+import controller.ProfessorController;
+import model.Professor;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ProfessorController professorController = new ProfessorController();
+        Professor p = new Professor();
+
+        System.out.println("Digite o nome do professor");
+        p.setNome(Scanner.next());
+        System.out.println("Digite a idade do professor");
+        p.setIdade(Scanner.nextInt());
+        System.out.println("Digite a especializacao do professor");
+        p.setEspecializacao(Scanner.nextLine());
+
+
+
+    }
+}
---
+++ Aula08/src/model/Professor.java
@@ -0,0 +1,32 @@
+package model;
+
+import enums.Classificacao;
+import model.interfaces.Folha;
+
+public final class Professor extends Pessoa implements Folha {
+    private String especializacao;
+    //get e set para especializacao
+
+
+    public String getEspecializacao() {
+        return especializacao;
+    }
+
+    public void setEspecializacao(String especializacao) {
+        this.especializacao = especializacao;
+    }
+
+    @Override
+    public void quem_sou_eu() {
+        System.out.println(Classificacao.Professor);
+    }
+    @Override
+    public String toString() {
+        return super.toString()+ "especializacao=" + especializacao;
+    }
+
+    @Override
+    public void calcularSalario() {
+
+    }
+}
